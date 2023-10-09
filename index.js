4ca05adf88da9f77f2b6740b1e42b90d6b4f8fa8
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-09 14:58:18 -0700

    added initial files

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/dogs', (req, res) => {
+  res.send('Dogs are often considered better than cats by some because of their loyalty and strong companionship.');
+});
+
+app.listen(PORT, () => {
+  console.log('Cloud server is running!');
+});
